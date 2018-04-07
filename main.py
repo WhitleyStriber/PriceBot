b0f2541ddedc4350a02f53724c9dd03bcb8a87d4
Drew <andrewtramutolo@gmail.com>
2018-04-07 03:44:02 -0400

    Working

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import json
 
 ws = create_connection("wss://www.bitmex.com/realtime?subscribe=trade,orderBook:orderBookL2")
 
+priceAmount = None;
+
 while 1:
     result =  ws.recv()
     data = json.loads(result)
-    # for key, value in data.items():
-    #     # print (key, value)
-
     if 'data' in data:
-        # print(data['data'])
-        print(data.get('data'))
+        timestamp = str(data['data'][0]['timestamp'])
+        symbol = str(data['data'][0]['symbol'])
+        size = int(data['data'][0]['size'])
+        price = int(data['data'][0]['price'])
+        side = str(data['data'][0]['side'])
+
+        priceAmount = 999999
+
+        # Buy Side
+        if (symbol == 'XBTUSD') & (price >= priceAmount) & (side == "Buy"):
+            print ("{0} BitMEX {1} {2} contracts market bought at {3}".format(timestamp, symbol, size, price))
+
+        if (symbol == 'XBTUSD') & (price >= priceAmount) & (side == "Sell"):
+            print ("{0} BitMEX {1} {2} contracts market sold at {3}".format(timestamp, symbol, size, price))
 
 
-
-ws.close()
+ws.run_forever()
